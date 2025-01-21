ba580a3e78c0c4df7bce3e74cabcb5e151d08b7f
jdulera <jdulera@bitglass.com>
2025-01-21 20:56:53 +0530

    Create File model and serializer

--- filesharing_backend/filemanager/models.py
+++ filesharing_backend/filemanager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class File(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files')
+    file = models.FileField(upload_to='uploads/')
+    shared_with = models.ManyToManyField(User, related_name='shared_files', blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file.name
