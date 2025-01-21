2254782c9edfb71dc61451ad8b104ac24943ef82
jdulera <jdulera@bitglass.com>
2025-01-21 21:00:31 +0530

    Create views and append to url patterns

--- filesharing_backend/filemanager/views.py
+++ filesharing_backend/filemanager/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets, permissions
+from .models import File
+from .serializers import FileSerializer
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return File.objects.filter(owner=self.request.user) | File.objects.filter(shared_with=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
